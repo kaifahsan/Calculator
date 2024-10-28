0df4c7153a78bd5260ece54d595d6786cb9c6226
Muhammad Kaif <kaif88679@gmail.com>
2024-10-28 15:41:39 +0000

    first commit

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
-let operatorShown =document.getElementById("operator");
-if (operatorShown === '+' || '-' || '/' || '*') {
-    display.value;
+function firstFunction() {
+     display.value= eval(display.value)
+}  
+let lastInput ='';
+function addOperator(value){
+   const operators=['+','-','/','*'];
+   if(operators.includes(lastInput) && operators.includes(value)){
+    return;
+   }
+   display.value += value;
+   lastInput =value;
 }
